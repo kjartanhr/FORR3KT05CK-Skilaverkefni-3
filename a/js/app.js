590292135d6411af661de370cbfcd3968fbe4896
Kjartan Hrafnkelsson <kjartan@kjartan.io>
2022-09-21 10:51:06 +0000

    Update app.js

--- a/js/app.js
+++ a/js/app.js
@@ -1,5 +1,7 @@
+// gera date object
 const date = new Date();
 
+// nöfn daganna
 const days = [
     "sunnudagur",
     "mánudagur",
@@ -10,14 +12,22 @@ const days = [
     "laugardagur"
 ]
 
+// sýna daginn
 document.querySelector('#day').innerText = days[date.getDay()];
 
+// fall til að uppfæra klukkuna
 function updateTime() {
+    // búa til nýtt data object alltaf þegar fallið er kallað
     const date = new Date();
+    // velja klukkuna í html
     const el = document.querySelector('#time');
 
+    // uppfæra klukkuna. við breytum tölunum í streng með String.prototype.toString()
+    // við bætum '0' fyrir framan þær tölur sem eru eins stafa með String.prototype.padStart()
     el.innerText = `${date.getHours().toString().padStart(2, '0')}:${date.getMinutes().toString().padStart(2, '0')}:${date.getSeconds().toString().padStart(2, '0')}`
 }
 
+// keyra þegar síðan er opnuð
 updateTime();
+// keyra á hverri sekúndu
 setInterval(updateTime, 1000);
